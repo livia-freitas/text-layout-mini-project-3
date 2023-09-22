c1e4100160051968174d516e498b407f865d63da
Livia S. Freitas <138071569+livia-freitas@users.noreply.github.com>
2023-09-22 13:55:33 -0500

    Truncated mostly working

--- Truncated.java
+++ Truncated.java
@@ -1,11 +1,17 @@
 package lab.polymorphism;
 
-public class Truncated implements TextBlock{
+/**
+ * Author: Livia Stein Freitas
+ * Truncates a TextBlock to a specified width.
+ */
+
+public class Truncated implements TextBlock {
   TextBlock contents;
   int new_width;
 
-  public Truncated(TextBlock contents, int new_width){
-
+  public Truncated(TextBlock _contents, int _new_width) {
+    this.contents = _contents;
+    this.new_width = _new_width;
   }
 
   /**
@@ -14,23 +20,29 @@ public class Truncated implements TextBlock{
    * @pre i < this.height()
    * @exception Exception if the row number is invalid.
    */
-  public String row(int i) throws Exception{
-
+  public String row(int i) throws Exception {
+    int h = this.contents.height();
+    if ((i >= 0) && (i <= h)) {
+      return this.contents.row(i).substring(0, this.new_width);
+    }
+    // Everything else
+    else {
+      throw new Exception("Invalid row " + i);
+    }
   }
 
   /**
    * Determine how many rows are in the block.
    */
-  public int height(){
-
-  
+  public int height() {
+    return this.contents.height();
   }
 
   /**
    * Determine how many columns are in the block.
    */
-  public int width(){
-
+  public int width() {
+    return this.contents.width();
   }
 }
 
